a556b1d40207f49be713db4922ec5bac4e5560f8
AliciaMayor <91277293+AliciaMayor@users.noreply.github.com>
2022-12-06 11:55:38 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -27,23 +27,18 @@ public class Main {
 		
 		game.initializateArrays();
 		
+		/*IDEA: 
+		 * · Si lo que pasamos por parametro a los metodos de encontrar solucion es la ultima posicion, hemos encontrado solucion
+		 * · Si todos los metodos dan falso, backtracking
+		 * · Si uno o varios metodos dan true, comprobar en el array posibles soluciones cual es la mayor lexicografica
+		 * 		y esa es la posicion que pasaremos a los metodos en la siguiente pasada
+		 * */
+		boolean solution = game.checkUp(1, 1);
+		System.out.print(solution);
+		
 		scan.close();
 		
-//		/*COMPROBACIÓN DE QUE ESTOY GUARDANDO BIEN LOS DATOS EN LA MATRIZ ARRAYDATA*/
-//		int[][] arrayDataTest = game.getArrayData();
-//		int dimension = arrayDataTest.length;
-//		
-//		for(int i=0; i<dimension; i++) {
-//			
-//			for(int j=0; j<dimension; j++) {
-//				
-//				System.out.print(arrayDataTest[i][j] + " ");
-//			}
-//			
-//			System.out.print("\n");
-//		}
-//		/* FIN COMPROBACIÓN DE QUE ESTOY GUARDANDO BIEN LOS DATOS EN LA MATRIZ ARRAYDATA*/
-		
+
 		/* COMPROBACIÓN DE QUE ESTOY GUARDANDO BIEN LOS DATOS  EL ARRAYLIST*/
 		System.out.println("\n---------------Comprobación arrayList---------------\n");
 		
@@ -64,6 +59,7 @@ public class Main {
 		}
 		/* FIN COMPROBACIÓN DE QUE ESTOY GUARDANDO BIEN LOS DATOS EN EL ARRAYLIST*/
 		
+		
 		/* COMPROBACIÓN DE QUE COJO BIEN LAS FILAS Y COLUMNAS COMO ATRIBUTOS DE CLASE*/
 		System.out.println("\n---------------Comprobación filas y columnas ---------------\n");
 		
@@ -72,6 +68,7 @@ public class Main {
 				
 		System.out.println("Filas: " + rows + "  Columnas: " + columns);
 		/* FIN COMPROBACIÓN DE QUE COJO BIEN LAS FILAS Y COLUMNAS COMO ATRIBUTOS DE CLASE*/
+		
 		
 		/* COMPROBACIÓN DE QUE COJO BIEN LAS FILAS Y COLUMNAS COMO ATRIBUTOS DE CLASE*/
 		System.out.println("\n---------------Comprobación maximo y minimo---------------\n");
@@ -83,7 +80,82 @@ public class Main {
 		/* FIN COMPROBACIÓN DE QUE COJO BIEN LAS FILAS Y COLUMNAS COMO ATRIBUTOS DE CLASE*/
 		
 		
-		//COMPROBAR TODOS LOS ARRAYS 
+		/* COMPROBACIÓN DE QUE ESTOY GUARDANDO BIEN LOS DATOS  EL ARRAYDATA*/
+		System.out.println("\n---------------Comprobación arrayData---------------\n");
+		
+		int[][] arrayData = new int[rows][columns];
+		arrayData = game.getArrayData();
+		
+		for(int z=0; z<rows; z++) {
+			
+			for(int x=0; x<columns; x++) {
+				
+				System.out.print(arrayData[z][x] + " ");
+			}
+			
+			System.out.print("\n");
+		}
+		
+		/* FIN COMPROBACIÓN DE QUE ESTOY GUARDANDO BIEN LOS DATOS EN EL ARRAYDATA*/ 
+		
+		
+		/* COMPROBACIÓN DE QUE ESTOY GUARDANDO BIEN LOS DATOS  EL ARRAYDATABACKUP*/
+		System.out.println("\n---------------Comprobación arrayDataBackUp---------------\n");
+		
+		int[][] arrayDataBackUp = new int[rows][columns];
+		arrayDataBackUp = game.getArrayDataBackUp();
+		
+		for(int v=0; v<rows; v++) {
+			
+			for(int b=0; b<columns; b++) {
+				
+				System.out.print(arrayDataBackUp[v][b] + " ");
+			}
+			
+			System.out.print("\n");
+		}
+		
+		/* FIN COMPROBACIÓN DE QUE ESTOY GUARDANDO BIEN LOS DATOS EN EL ARRAYDATABACKUP*/ 
+		
+		
+		/* COMPROBACIÓN DE QUE ESTOY GUARDANDO BIEN LOS DATOS  EL ARRAYDATAWITHSPACES*/
+		System.out.println("\n---------------Comprobación arrayDataWithSpaces---------------\n");
+		int rowsPlus = rows + (rows-1);
+		int columnsPlus = columns + (columns-1);
+		
+		String[][] arrayDataWithSpaces = new String[rowsPlus][columnsPlus];
+		arrayDataWithSpaces = game.getArrayDataWithSpaces();
+		
+		for(int p=0; p<rowsPlus; p++) {
+			
+			for(int o=0; o<columnsPlus; o++) {                                                                          
+				
+				System.out.print(arrayDataWithSpaces[p][o] + " ");
+			}
+			
+			System.out.print("\n");
+		}
+		
+		/* FIN COMPROBACIÓN DE QUE ESTOY GUARDANDO BIEN LOS DATOS EN EL ARRAYDATAWITHSPACES*/ 
+		
+		
+		/* COMPROBACIÓN DE QUE ESTOY GUARDANDO BIEN LOS DATOS  EL ARRAYPOSIBLESOLUTIONS*/
+		System.out.println("\n---------------Comprobación arayPosibleSolutions---------------\n");
+		
+		int[][] arrayPosibleSolutions =  new int[8][2];
+		arrayPosibleSolutions = game.getArrayPosibleSolutions();
+		
+		for(int f=0; f<8; f++) {
+			
+			for(int d=0; d<2; d++) {
+				
+				System.out.print(arrayPosibleSolutions[f][d] + " ");
+			}
+			
+			System.out.print("\n");
+		}
+		
+		/* FIN COMPROBACIÓN DE QUE ESTOY GUARDANDO BIEN LOS DATOS EN EL ARRAYPOSIBLESOLUTIONS*/ 
 	}
 	
 	
--- Puzzle.java
+++ Puzzle.java
@@ -18,6 +18,17 @@ public class Puzzle {
 	private int[][] arrayDataBackUp; //datos que introduce el usuario en formato array para backup en el backtrackin
 	int min = 10; //numero minimo introducido por el usuario
 	int max = 0;; //numero maximo introducido por el usuario
+	private int[][] posibleSolutions = new int[8][2]; //Array que guarda las posibles posiciones solucion
+	/* FORMA DEL ARRAY DE POSIBLESSOLUCIONES: 
+	 *   [i][j]  Arriba
+	 *   [i][j]  Abajo
+	 *   [i][j]  Derecha
+	 *   [i][j]  Izquierda
+	 *   [i][j]  Arriba-Derecha
+	 *   [i][j]  Arriba-Izquierda
+	 *   [i][j]  Abajo-Derecha
+	 *   [i][j]  Abajo-Izquierda
+	 */  
 	
 	/**
 	* Getter
@@ -26,6 +37,42 @@ public class Puzzle {
 	public ArrayList<int[]> getArrayListData() {
 		
 		return this.arrayListData;
+	}
+	
+	/**
+	* Getter
+	* @return arrayData: the array where the input data is saved
+	*/
+	public int[][] getArrayData() {
+		
+		return this.arrayData;
+	}
+	
+	/**
+	* Getter
+	* @return arrayDataBackUp: the backup array where the input data is saved
+	*/
+	public int[][] getArrayDataBackUp() {
+		
+		return this.arrayDataBackUp;
+	}
+	
+	/**
+	* Getter
+	* @return arrayDataWithSpaces: the backup array where the input data is saved + spaces to draw the lines
+	*/
+	public String[][] getArrayDataWithSpaces() {
+		
+		return this.arrayDataWithSpaces;
+	}
+	
+	/**
+	* Getter
+	* @return arrayPosibleSolutions: the backup array with the posible solutions
+	*/
+	public int[][] getArrayPosibleSolutions() {
+		
+		return this.posibleSolutions;
 	}
 	
 	/**
@@ -240,7 +287,9 @@ public class Puzzle {
 		int mArrayList = 0;
 		this.arrayData = new int[this.rows][this.columns];
 		this.arrayDataBackUp = new int[this.rows][this.columns];
-		this.arrayDataWithSpaces = new String[this.rows + (this.rows - 1)][this.columns + (this.columns - 1)];
+		int rowsAmplified = this.rows + (this.rows - 1);
+		int columnsAmplified = this.columns + (this.columns - 1);
+		this.arrayDataWithSpaces = new String[rowsAmplified][columnsAmplified];
 		
 		for(int i=0; i<this.rows; i++) {
 			
@@ -251,9 +300,9 @@ public class Puzzle {
 			}
 		}
 		
-		for(int m=0; m<(this.rows + (this.rows - 1)); m=+2) {
+		for(int m=0; m<rowsAmplified; m=m+2) {
 			
-			for(int n=0; n<(this.columns + (this.columns - 1)); n=+2) {
+			for(int n=0; n<columnsAmplified; n=n+2) {
 				
 				this.arrayDataWithSpaces[m][n]=  String.valueOf(this.arrayListData.get(mArrayList)[nArrayList]);
 				nArrayList++;		
@@ -262,5 +311,80 @@ public class Puzzle {
 			nArrayList = 0;
 			mArrayList++;
 		}
+		
+		for(int o=0; o<rowsAmplified; o++) {
+			
+			for(int p=0; p<columnsAmplified; p++) {
+				
+				if(this.arrayDataWithSpaces[o][p]==null) {
+					
+					this.arrayDataWithSpaces[o][p] = " ";
+				}
+			}
+		}
+		
+		//La matriz de pposibles soluciones la inicializamos a -1, ya que es un valor que no es posible en la matriz 
+		for(int h=0; h<8; h++) {
+			
+			for(int g=0; g<2; g++) {
+				
+				this.posibleSolutions[h][g] = -1;
+			}
+		}
 	}
+	
+	/**
+	* Method that chekcs if a posible solution is to go up
+	* @param i, row in which we are situated
+	* @param j, column in which we are situated
+	* @return boolean, true if it is a posible solution, false in casi it is not
+	*/
+	public boolean checkUp(int i, int j) {
+		
+		boolean posibleSolution =  false;
+		int numberActualPosition = this.arrayData[i][j];
+		
+		//Para i=0 no podemos ir hacia arriba porque se sale fuera de los bordes del array
+		if(i!=0) {
+			
+			//Guardamos el numero que hay en la posicion de arriba
+			int numberUp = this.arrayData[i-1][j];
+			
+			//Si la posicion actual es el numero maximo del array, hay que buscar que el siguiente sea el minimo
+			if(numberActualPosition == this.max) {
+				
+				//Chequeamos que la posicion a la que vamos no sea 0, ya que eso indicaria que ya hemos pasado por ahi
+				if((numberUp== this.min) && (numberUp!=0)) {
+					
+					posibleSolution = true;
+					
+					//Guardamos la posible solución para al tener todas calculadas elegir la que es mayor lexicograficamente
+					this.posibleSolutions[0][0] = i-1;
+					this.posibleSolutions[0][1] = j;
+					
+					this.arrayData[i][j]=0; //Si es solucion ponemos a 0 la posicion actual para no volver a ella
+				}
+			}
+			//Sino, nos basta con que sea una unidad más que el actual
+			else {
+				
+				if((numberUp == numberActualPosition+1) && (numberUp!=0)) {
+					
+					posibleSolution = true;
+					
+					this.posibleSolutions[0][0] = i-1;
+					this.posibleSolutions[0][1] = j;
+					
+					this.arrayData[i][j]=0;
+				}
+			}
+		}
+		
+		return posibleSolution;
+	}	
+	
+	
 }
+
+
+
